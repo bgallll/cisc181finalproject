e79ff54b4a96a6128f71a41bcd56164889b75f70
bgallll <baxterg@udel.edu>
2024-05-01 11:01:42 -0400

    main

--- src/app/box-editor/box-editor.component.test.ts
+++ src/app/box-editor/box-editor.component.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, test, beforeAll } from "@jest/globals";
-import { MainComponent } from "./main.component";
+import { BoxEditorComponent } from "./box-editor.component";
 import { bootstrap } from "@gsilber/webez";
 
-describe("MainComponent", () => {
+describe("BoxEditorComponent", () => {
     let component: any = undefined;
     beforeAll(() => {
         const html: string = `<div>Testing Environment</div><div id='main-target'></div>`;
-        component = bootstrap<MainComponent>(MainComponent, html);
+        component = bootstrap<BoxEditorComponent>(BoxEditorComponent, html);
     });
     describe("Constructor", () => {
         test("Create Instance", () => {
-            expect(component).toBeInstanceOf(MainComponent);
+            expect(component).toBeInstanceOf(BoxEditorComponent);
         });
     });
 });
--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -1,86 +1,19 @@
 import html from "./main.component.html";
 import css from "./main.component.css";
 import { EzComponent, BindValue } from "@gsilber/webez";
-
-interface SummaryStatistics {
-    mean: number;
-    median: number;
-    mode: number;
-    range: number;
-    dataSetSize: number;
-    standardDeviation: number;
-    variance: number;
-    iqr: number;
-}
+import { BoxEditorComponent } from "./box-editor/box-editor.component";
 
 export class MainComponent extends EzComponent {
-    numberList: string = "";
-    summaryStatistics: SummaryStatistics = {
-        mean: 0,
-        median: 0,
-        mode: 0,
-        range: 0,
-        dataSetSize: 0,
-        standardDeviation: 0,
-        variance: 0,
-        iqr: 0,
-    };
-
-    calculateStatistics() {
-        const numbers: number[] = this.numberList.split(",").map(Number);
-        this.summaryStatistics.mean = this.calculateMean(numbers);
-        this.summaryStatistics.median = this.calculateMedian(numbers);
-        this.summaryStatistics.range = this.calculateRange(numbers);
-        this.summaryStatistics.dataSetSize = numbers.length;
-        this.summaryStatistics.standardDeviation =
-            this.calculateStandardDeviation(numbers);
-        this.summaryStatistics.variance = this.calculateVariance(numbers);
-        this.summaryStatistics.iqr = this.calculateIQR(numbers);
-    }
-
-    generateGraph() {}
-
-    private calculateMean(numbers: number[]): number {
-        return numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
-    }
-
-    private calculateMedian(numbers: number[]): number {
-        const sortedNumbers: number[] = numbers.sort((a, b) => a - b);
-        const mid: number = Math.floor(sortedNumbers.length / 2);
-        return sortedNumbers.length % 2 === 0 ?
-                (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2
-            :   sortedNumbers[mid];
-    }
-
-    private calculateRange(numbers: number[]): number {
-        // Calculate the range (max - min)
-        return Math.max(...numbers) - Math.min(...numbers);
-    }
-
-    private calculateStandardDeviation(numbers: number[]): number {
-        const variance: number = this.calculateVariance(numbers);
-        return Math.sqrt(variance);
-    }
-
-    private calculateVariance(numbers: number[]): number {
-        const mean: number = this.calculateMean(numbers);
-        return this.calculateMean(
-            numbers.map((num) => Math.pow(num - mean, 2)),
-        );
-    }
-
-    private calculateIQR(numbers: number[]): number {
-        const sortedNumbers: number[] = numbers.sort((a, b) => a - b);
-        const mid: number = Math.floor(sortedNumbers.length / 2);
-        const q1: number = this.calculateMedian(sortedNumbers.slice(0, mid));
-        const q3: number = this.calculateMedian(sortedNumbers.slice(mid + 1));
-        return q3 - q1;
-    }
-
     @BindValue("example-target")
     private myText: string = "";
 
+    private boxEditor = new BoxEditorComponent();
+
+    box: BoxEditorComponent;
+
     constructor() {
         super(html, css);
+        this.addComponent(this.boxEditor, "box");
+        this.box = this.boxEditor;
     }
 }
